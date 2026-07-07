e73121bf4769e9f9aa1595d60f2860b732c3c1d4
sadmanshahriar305 <sadman.shahriar305@gmail.com>
2026-07-07 09:44:07 +0600

    Update student information

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main() {
     // Student Information
     // ==========================
 
-    string name = "Your Name";
-    string id = "24-XXXXX-X";
-    string section = "A";
+    string name = "Sadman Shahriar";
+    string id = "24-57470-2";
+    string section = "M";
 
     // ==========================
     // Personal Information
